5aeb405f3169aff8e540ab4c2e315c1b79ec60c6
CMD <80255379+iCMDgithub@users.noreply.github.com>
2022-09-02 17:17:45 +0300

    Update CustomCharacterI2C.py

--- Examples/CustomCharacterI2C.py
+++ Examples/CustomCharacterI2C.py
@@ -19,41 +19,41 @@ The following configuration is the default one:
 
 display.writeString("Custom Characters: ", delay = 0.1, newlineDelay = 0.3) # write a string with the specified delays. 
 
-# New Character
+# Custom Characters
 
-# Function is defines as: display.newCharacter(characterArray, address)
-display.newCharacter([0b00000,
+# Function is defines as: display.NewCharacter(characterArray, address)
+display.NewCharacter([0b00000,
                       0b01010,
                       0b01010,
                       0b00000,
                       0b10001,
                       0b10001,
                       0b01110,
-                      0b00000], 0) # Created a smiley face at address 0
+                      0b00000], 1) # Created a smiley face at address 0
 
-display.newCharacter([0b00000,
+display.NewCharacter([0b00000,
                       0b01110,
                       0b11111,
                       0b01110,
                       0b00100,
                       0b00100,
                       0b11111,
-                      0b00000], 1) # Created a tree at address 1
+                      0b00000], 2) # Created a tree at address 1
 
 # Function is defines as: display.writeChar(character_address)
-display.writeChar(0)
 display.writeChar(1)
+display.writeChar(2)
 
 # Fun fact: you can use writeString() to write custom characters as well, by placing them in an array! The array can also contain strings as elements.
-display.writeString(["Writing some custom characters (again): ", 0, 1, "\nHow cool is that!?"], lineNum = 2, columnNum = 0, delay = 0.1, newlineDelay = 0.3)
+display.writeString(["Writing some custom characters (again): ", 1, 2, "\nHow cool is that!?"], lineNum = 2, columnNum = 0, delay = 0.1, newlineDelay = 0.3)
 
 """
 Useful things to know:
-1) You can only create 8 characters (hardware limit)
+1) You can only create 8 characters for 5x8 fonts, and 4 for 5x10 fonts (hardware limit)
 2) Each custom character is saved by the display at a specific address in it's memory
-3) The custom character's memory address can range from 0 to 8 for 5x8 characters, and 4 for 5x10. You'll need use this address to save a new
+3) The custom character's memory address can range from 1 to 8s. You'll need use this address to save a new
    character (using the newCharacter() method), and print it on the display (with writeChar())
-4) The newCharacter() method takes an array of (binary) integers as shown above. 1 means the pixel should be on, and 0 means it should be off.
+4) The NewCharacter() method takes an array of (binary) integers as shown above. 1 means the pixel should be on, and 0 means it should be off.
 5) Usually, HD44780 displays use 8 pixel width x 5 pixel height characters, but others use 5x10. Check your display to make sure which one you should use.
 6) The writeChar() method will write a character with the character code (address) provided as an argument. 
    This means that if you send an address not equal to a custom character address, the display will print the character with the equivalent
