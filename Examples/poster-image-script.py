9697d0cb6aadf4865c64f087689ae125f5abef4f
CMD <80255379+iCMDgithub@users.noreply.github.com>
2022-09-02 20:20:12 +0300

    Update poster-image-script.py

--- Examples/poster-image-script.py
+++ Examples/poster-image-script.py
@@ -10,6 +10,15 @@ display.NewCharacter([0b00000,
                       0b10001,
                       0b01110,
                       0b00000], 0) # create a new custom character with code 0
-                      
+
+display.NewCharacter([0b00000,
+                      0b01010,
+                      0b01010,
+                      0b00000,
+                      0b10001,
+                      0b10001,
+                      0b01110,
+                      0b00000], 1) # create a new custom character with code 1
+
 display.writeString("Hello world!")
-display.writeString(["I'm using CharPi HD44780 Library!", 0], lineNum = 1, columnNum = 0)
+display.writeString(["I'm using CharPi \nHD44780 Library! ", 0, "\nHow cool is that!", 1], lineNum = 1, columnNum = 0)
